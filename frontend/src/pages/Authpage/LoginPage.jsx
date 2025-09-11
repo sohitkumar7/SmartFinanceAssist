34eb6988c32af7ef9ce0dc222de4288151cd9eab
Sohit Kumar <sohit3165@gmail.com>
2025-09-12 00:21:52 +0530

    updated

--- frontend/src/pages/Authpage/LoginPage.jsx
+++ frontend/src/pages/Authpage/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/clerk-react";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
@@ -11,74 +11,122 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../../components/ui/card"
+} from "../../components/ui/card";
 
-import { Input } from "../../components/ui/input"
-import { Label } from "../../components/ui/label"
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
 
 function LoginPage() {
   const navigate = useNavigate();
-  return (
-    <div>
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    // console.log(username);
+    console.log(email);
+    console.log(password);
+    // console.log(ConfirmPassword);
+
+    // if(password !== ConfirmPassword){
+    //   toast.error("password and confirmpassword donnot match")
+    // }
+    // else{
+
+    //   const userinfo = {
+    //     name:username,
+    //     password:password,
+    //     email:email,
+    //     confirmpassword:ConfirmPassword
+    //   }
+
+    //   await axios
+    //     .post("api/user/register",userinfo)
+    //     .then((response) => {
+    //       if(response.data){
+    //         toast.success("Register Successfully");
+    //       }
+    //       localStorage.setItem("spotify",JSON.stringify(response.data))
+    //       setAuthUser(response.data);
+    //       // ther we have to set current user which is logged in
+    //     })
+    //     .catch((error) => {
+    //       if(error.response){
+    //         console.log(error);
+    //         toast.error("Error : "+error.response.data.message)
+    //       }
+    //     })
+
+    // }
+  };
+
+  return (
+    <>
       <nav className="sticky  top-0  w-full flex justify-between border-b-2 border-blue-500 ">
-        <img onClick={() => navigate("/")}  className="h-12" src={logo} alt="" />
+        <img onClick={() => navigate("/")} className="h-12" src={logo} alt="" />
         <Button
           onClick={() => navigate("/signup")}
           className="m-2 mr-5 bg-white text-black hover:bg-black hover:text-white"
-          >
+        >
           Signup
         </Button>
       </nav>
 
-      <Card className="w-full max-w-sm ml- ">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-                />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                  Forgot your password?
-                </a>
+      <div className="flex mt-30  items-center justify-center">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+            <CardAction>
+              <Button variant="link">Sign Up</Button>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={submitHandler}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
               </div>
-              <Input id="password" type="password" required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-      </CardFooter>
-    </Card>
-    
-</div>
+              <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full mt-4">
+                  Login
+                </Button>
+              </CardFooter>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
 
--- frontend/src/pages/Authpage/SignupPage.jsx
+++ frontend/src/pages/Authpage/SignupPage.jsx
@@ -3,6 +3,18 @@ import { Button } from "../../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { SignUp } from "@clerk/clerk-react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
+
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
 
 function SignupPage() {
   const navigate = useNavigate();
@@ -10,6 +22,49 @@ function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [ConfirmPassword, setConfirmPassword] = useState("");
+
+
+   const submitHandler = async(e) => {
+
+    e.preventDefault();
+
+
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(ConfirmPassword)
+
+    // if(password !== ConfirmPassword){
+    //   toast.error("password and confirmpassword donnot match")
+    // }
+    // else{
+
+    //   const userinfo = {
+    //     name:username,
+    //     password:password,
+    //     email:email,
+    //     confirmpassword:ConfirmPassword
+    //   }
+      
+    //   await axios
+    //     .post("api/user/register",userinfo)
+    //     .then((response) => {
+    //       if(response.data){
+    //         toast.success("Register Successfully");
+    //       }
+    //       localStorage.setItem("spotify",JSON.stringify(response.data))
+    //       setAuthUser(response.data);
+    //       // ther we have to set current user which is logged in
+    //     })
+    //     .catch((error) => {
+    //       if(error.response){
+    //         console.log(error);
+    //         toast.error("Error : "+error.response.data.message)
+    //       }
+    //     })
+    
+    // }
+  };
 
   return (
     <div className="min-h-screen outer-overflow ">
@@ -23,53 +78,79 @@ function SignupPage() {
         </Button>
       </nav>
 
-      <form className=" mt-70 flex flex-col justify-center items-center border-2  border-black  rounded  ml-[43%] h- w-60">
-        
-        <div className="mb-4 border-2">
-          <label className="block text-sm font-medium mb-1"> UserName</label>
-          <input
-            type="text"
-            placeholder="Username"
-            className="auth-input"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        
-        <div className="mb-4">
-          <label className="block text-sm font-medium mb-1" > email</label>
-          <input
-            type="email"
-            placeholder="email"
-            className="auth-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="p-2"> password </label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="auth-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="p-2"> Confirm Password </label>
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            className="auth-input"
-            value={ConfirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <Button> Register </Button>
-      </form>
+      <div className="flex mt-30  items-center justify-center">
+        <Card onSubmit={submitHandler} className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>SignUp</CardTitle>
+            <CardDescription>
+              Enter your Details to signUp in Welth
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form  onSubmit={submitHandler}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="text">Username</Label>
+                  <Input
+                    type="text"
+                    placeholder="UserName"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">ConfirmPassword</Label>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    value={ConfirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+            
+          <CardFooter className="flex-col gap-2 mt-4">
+            <Button type="submit" className="w-full">
+              SignUp
+            </Button>
+          </CardFooter>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
